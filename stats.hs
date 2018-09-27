64be043b5b758351ecb40a702d1cdd80b838cba5
Josh Gordon <joshua.sam.gordon@gmail.com>
2018-09-27 03:00:55 -0400

    Remove superfluous code; add gitignore; add documentation

--- stats.hs
+++ stats.hs
@@ -1,17 +1,22 @@
 module Stats where
 
+--factorial
 fact :: Int -> Int
 fact n = product [1..n]
 
+--binomial coefficient function
 choose :: Int -> Int -> Int
 choose n k = (fact n) `div` ((fact k) * (fact (n-k)))
 
+--pdf for binomial distribution
 binomial :: Double -> Int -> Int -> Double
 binomial p k n = (fromIntegral (choose n k)) * (p^n) * (1-p)^(n-k)
 
+--pdf for poisson distribution
 poisson :: Double -> Int -> Double
 poisson l n = exp(-l) * (l^n) / fromIntegral (fact n)
 
+--pdf for uniform distribution
 uniform :: Double -> Double -> Double -> Double
 uniform a b _ = 1/(b-a)
 
@@ -24,26 +29,3 @@ cdf f n = sum $ map f [0..n]
 --can only use 1 type
 cdf' :: (Enum a, Num a) => (a -> a) -> a -> a -> a
 cdf' f n p = sum $ map (*p) $ map f [0,p..n]
-
-
-
-
-
-(///) :: [a] -> [a] -> [a]
-[] /// ys = ys
-(x:xs) /// ys = x : (ys /// xs)
-
-
-powerset :: [a] -> [[a]]
-powerset [] = [[]]
-powerset (x:xs) = powerset xs /// map (x:) (powerset xs)
-
-fix :: (a -> a) -> a
-fix f = let x = f x in x
-
-lengthDI :: ([a] -> Int) -> [a] -> Int
-lengthDI _ [] = 0
-lengthDI f (_:xs) = 1 + f xs
-
-realLength :: [a] -> Int
-realLength = fix lengthDI
